e8a28dea69400fc671677d09794c4eefafe9cb8f
POOS2 <mg.blaschkel@uea.edu.ec>
2025-01-05 12:47:56 -0500

    sube un nuevo archivo

---
+++ Unidad 1/1.2. Programacion tradicional frente a POO/1.2-2. Ejemplo Programacion tradicional.py
@@ -0,0 +1,27 @@
+# Programacion tradicional
+# Ejemplo: procedimiento de encendido de un coche
+
+#Descripcion
+tanque = 'lleno'
+arrancado = 'false'
+
+#funcion de para encende un coche
+#comprobamos el arranque
+def arrancar(false):
+    global arrancado
+    arrancado = false
+
+def arrancar(true):
+    global arrancado
+    arrancado = true
+#lleno y arrancamos
+def encendemos(true):
+    global  tanque, arrancado
+    tanque_completo = true / tanque
+    if tanque_completo = tanque:
+        arrancado= true
+        print("verificamos", true, "conducimos")
+
+#PROGRAMACION TRADICIONAL
+arrancar(100)
+encendemos(100)
